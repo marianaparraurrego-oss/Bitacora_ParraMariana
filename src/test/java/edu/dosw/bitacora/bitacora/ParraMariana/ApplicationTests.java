f1b0b5ab797b580678bca8cebee581c95a5b00fe
Mariana Parra <andrea.parra-u@mail.escuelaing.edu.co>
2026-02-01 21:49:40 -0500

    feature: estructura bitacora semanal

---
+++ src/test/java/edu/dosw/bitacora/bitacora/ParraMariana/ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.bitacora.bitacora.ParraMariana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
